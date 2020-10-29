0055b11c97b5b9cffe39524792a20c4fe92659e2
NathanLabbe <nathanlab1997@gmail.com>
2020-10-29 16:29:47 +0100

    class

--- src/Shingling.py
+++ src/Shingling.py
@@ -1,2 +1,13 @@
-f = open("../Datas/accuracy_garmin_nuvi_255W_gps.txt", "r")
-print(f.read()) 
+class Shingling:
+
+    def __init__(self, name, k):
+        self.doc = name
+        self.k   = k
+        print(self.doc)
+
+
+
+
+cl = Shingling("../Datas/accuracy_garmin_nuvi_255W_gps.txt",4)
+#f = open("../Datas/accuracy_garmin_nuvi_255W_gps.txt", "r")
+#print(f.read()) 
