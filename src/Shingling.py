ffc2f3b17256e8fe8f7677290a629bec836e20db
NathanLabbe <nathanlab1997@gmail.com>
2020-10-30 16:47:00 +0100

    Test Hash

--- src/Shingling.py
+++ src/Shingling.py
@@ -2,11 +2,11 @@ from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql import Row
 from pyspark.ml.feature import NGram
+from pyspark.ml.feature import HashingTF
 sc = SparkContext('local')
 spark = SparkSession(sc)
 
 class Shingling:
-
     def __init__(self, name, k):
         self.doc = name
         self.k   = k
@@ -27,26 +27,59 @@ class Shingling:
 def split(word): 
     return [char for char in word]  
 
-
+#Open text file
 f = open("Datas/accuracy_garmin_nuvi_255W_gps.txt.data")
 
+#Split the text into char array
 letter = split(f)
 result =[]
 for l in letter :
     result = result + split(l)
 #print(result)
 
-
+#Datafram creation
 df = spark.createDataFrame([Row(inputTokens=result)])
-ngram = NGram(n=2)
-ngram.setInputCol("inputTokens")
-ngram.setOutputCol("nGrams")
 
-
-
-#ngram = NGram(n=2, inputCol="inputToken", outputCol="ngrams")
-
+#ngram creation
+#ngram = NGram(n=8)
+#ngram.setInputCol("inputTokens")
+#ngram.setOutputCol("nGrams")
+ngram = NGram(n=8, inputCol="inputTokens", outputCol="ngrams")
 ngramDataFrame = ngram.transform(df)
-ngramDataFrame.select("ngrams").show(truncate=100)
+#ngramDataFrame.select("ngrams").show(truncate=100)
+
+
+##TRY TO HASH (Pas foufou pour le moment)
+hashingTF = HashingTF(inputCol="ngrams", outputCol="features")
+hashingTF.setNumFeatures(10)
+
+hashingTF.transform(ngramDataFrame).head().features
+#SparseVector(10, {5: 1.0, 7: 1.0, 8: 1.0})
+
+hashingTF.setParams(outputCol="freqs").transform(ngramDataFrame).head().freqs
+#SparseVector(10, {5: 1.0, 7: 1.0, 8: 1.0})
+
+#params = {hashingTF.numFeatures: 5, hashingTF.outputCol: "vector"}
+
+#hashingTF.transform(ngramDataFrame, params).head().vector
+#SparseVector(5, {0: 1.0, 2: 1.0, 3: 1.0})
+
+#hashingTFPath = temp_path + "/hashing-tf"
+
+#hashingTF.save(hashingTFPath)
+
+#loadedHashingTF = HashingTF.load(hashingTFPath)
+
+#loadedHashingTF.getNumFeatures() == hashingTF.getNumFeatures()
+#True
+
+#loadedHashingTF.transform(ngramDataFrame).take(1) == hashingTF.transform(ngramDataFrame).take(1)
+#True
+
+#hashingTF.indexOf("b")
+#5
+
+
+
 #f = open("../Datas/accuracy_garmin_nuvi_255W_gps.txt", "r")
 #print(f.read()) 
