8e33435f58b8cc3729fcdec64ad479a3ae66458c
NathanLabbe <nathanlab1997@gmail.com>
2020-11-02 19:49:04 +0100

    push du jaccard pour l'union des datafrae lol

--- src/CompareSets.py
+++ src/CompareSets.py
@@ -1,9 +1,12 @@
 from os import listdir
 from os.path import isdir, join
 
-from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType, FloatType
 from pyspark.ml.feature import NGram, HashingTF, IDF, Tokenizer
 from pyspark.ml import Pipeline
+import pyspark.sql.functions as F
+from pyspark.sql import Column
+
 
 class CompareSets:
    
@@ -18,20 +21,27 @@ class CompareSets:
         #self.df.filter(self.df.id == 0).show(truncate=100)  #len(list(set(list1).intersection(list2)))
         #union = (len(list1) + len(list2)) - intersection
         #return float(intersection) / union
+
+
         s1 = set()
         s2 = set()
         first = True
-        for t in self.df.select("features").collect():
-            tmpS = set(t.features.indices)
-            if first:
-                s1 = tmpS
-                s2 = tmpS
-                first = False
-            else:
-                s1 = s1.union(tmpS)
-                s2 = s2.intersection(tmpS)
+        for t in self.df.select("id","features").collect():
+            tmpT = set(t.features.indices)
+            schema = StructType([StructField(str(t.id), FloatType(), True)])
+            tmpDf = self.spark.createDataFrame([],schema)
+            l = []
+            for u in self.df.select("id","features").collect():
+                tmpU = set(u.features.indices)
+                s1 = tmpT.union(tmpU)
+                s2 = tmpT.intersection(tmpU)
+                tmpDf = tmpDf.union(self.spark.createDataFrame([(len(s2)/len(s1))],[str(t.id)]))
+                #l += [len(s2)/len(s1)]
+                #([(t.id, len(s2)/len(s1))], ["id", str(u.id) ])
+            self.df.withColumn(t.id,tmpDf(str(t.id)))
+        self.df.show(truncate=100)
 
-        
-        print("Jaccard : " + str(len(s2)/len(s1)))
+    
+
         
     
