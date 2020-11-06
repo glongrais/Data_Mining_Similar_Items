d4416d3c5fc300c311cd7000c7d7ca56535bf8fe
DarkManPC <guillaume.longrais@orange.fr>
2020-11-06 12:34:26 +0100

    optimizing minhash

--- src/MinHashing.py
+++ src/MinHashing.py
@@ -23,15 +23,16 @@ class MinHashing:
     def booleanMatrix(self):
         SetShingling = set()
         Rows = self.df.count()
+        datas = self.df.collect()
         for i in range(Rows):
-            SetShingling = SetShingling.union(set(self.df.collect()[i]["features"].indices))
+            SetShingling = SetShingling.union(set(datas[i]["features"].indices))
         Setint = set()
         Setint = [int(i) for i in SetShingling]
 
         matrix = self.spark.createDataFrame(self.sc.parallelize(Setint), IntegerType(), "value")
 
         for i in range(Rows):
-            listsh = set(self.df.collect()[i]["features"].indices) 
+            listsh = set(datas[i]["features"].indices) 
             listcontains = F.udf(lambda value: value in listsh) 
             matrix = matrix.withColumn("doc "+str(i), listcontains(F.col("value")))
         
